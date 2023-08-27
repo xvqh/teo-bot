da838acb28d037926eab6573143d3fcd108e662d
sewiiiii <138306559+sewiiiii@users.noreply.github.com>
2023-08-27 22:53:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 module.exports = {
 
-    token: "MTEzOTUzMzA5MzczMzE1MDgwMA.G7i4Du.swwj2kD3JaoibXpk5hgwhMsXpj4F1X7U3AESC0",
-    botinvite: "https://discord.com/api/oauth2/authorize?client_id=1139533093733150800&permissions=8&scope=bot", // votre invitation
-    supportChannelId: '1071128391908085860', // salon pour recevoir les problèmes
-    inviteLink: "https://discord.gg/9aqM62VB", // Lien de votre support
-    mongodb: "",
+    token: "",
+    botinvite: "", // votre invitation
+    supportChannelId: '', // salon pour recevoir les problèmes
+    inviteLink: "", // Lien de votre support
     autorole: null,  // Ceci stockera l'ID du rôle à attribuer automatiquement
     autoroleState: 'off'  // Ceci stockera l'état de l'autorole (soit 'on' soit 'off')
 };
