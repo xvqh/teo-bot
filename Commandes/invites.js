4c33baf9f86214b04f6fbf17eadde0a30f440af8
sewiiiii <138306559+sewiiiii@users.noreply.github.com>
2023-08-27 23:00:58 +0200

    invites.js

--- Commandes/invites.js
+++ Commandes/invites.js
@@ -16,31 +16,31 @@ module.exports = {
     ],
   
     async run(bot, interaction) {
-      // Vérifier si l'interaction provient d'un serveur (guild)
+      
       if (!interaction.guild) {
         return interaction.reply("Cette interaction ne provient pas d'un serveur (guild).", { ephemeral: true });
       }
   
       try {
-        // Récupérer l'utilisateur spécifié dans l'option 'membre'
+       
         const user = interaction.options.getUser('membre');
   
-        // Vérifier si l'utilisateur est valide
+        
         if (!user) {
           return interaction.reply("L'utilisateur spécifié est invalide.", { ephemeral: true });
         }
   
-        // Récupérer les invitations du serveur (guild)
+        
         let invites = await interaction.guild.invites.fetch();
   
-        // Filtrer les invitations pour celles de l'utilisateur spécifié
+        
         invites = invites.filter(invite => invite.inviter.id === user.id);
   
-        // Calculer le nombre total d'invitations de l'utilisateur
+       
         let totalInvites = 0;
         invites.forEach(invite => totalInvites += invite.uses);
   
-        // Créer l'Embed pour afficher les informations
+        
         const embed = new EmbedBuilder()
           .setColor(0x2f3136)
           .setAuthor({ name: user.tag, iconURL: user.displayAvatarURL({ dynamic: true }) })
@@ -48,7 +48,7 @@ module.exports = {
           .setDescription(`${user.tag} a **${totalInvites}** invites.`)
           .setTimestamp();
   
-        // Répondre à l'interaction avec l'Embed
+        
         await interaction.reply({ embeds: [embed], ephemeral: true });
       } catch (error) {
         console.error("Une erreur s'est produite lors de la récupération des invitations :", error);
